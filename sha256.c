062c000e76f1083b7a323bc885bdbb195bada834
PaulC <paul.chakravarti@gmail.com>
2014-05-26 00:24:56 +0100

    sha256 fix

--- sha256.c
+++ sha256.c
@@ -67,8 +67,6 @@ int crypto_hash_sha256(unsigned char *out,const unsigned char *in,unsigned long 
   return 0;
 }
 
-typedef unsigned int uint32;
-
 static uint32 load_bigendian(const unsigned char *x)
 {
   return
