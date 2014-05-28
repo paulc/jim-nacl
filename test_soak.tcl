5f370ac7d2f108f17345bfbb9ab368a8a1ebaba7
PaulC <paul.chakravarti@gmail.com>
2014-05-28 11:37:21 +0100

    test_soak.tcl improvements

--- test_soak.tcl
+++ test_soak.tcl
@@ -40,8 +40,8 @@ proc test_box {l} {
     lassign [box_keypair] sender_pk sender_sk
     lassign [box_keypair] recipient_pk recipient_sk
     set data [ randombytes $l ]
-    set c [ box $recipient_pk $sender_sk $data ]
-    if { [ box_open $sender_pk $recipient_sk {*}$c ] == $data } {
+    lassign [ box $recipient_pk $sender_sk $data ] n c
+    if { [ box_open $sender_pk $recipient_sk $n $c ] == $data } {
         puts "  box-$l: OK"
     } else {
         puts "  box-$l: FAILED"
@@ -59,11 +59,46 @@ proc test_sign {l} {
     }
 }
 
-for {set i 0} {$i < 1000} {incr i} {
-    puts "=== Count: $i"
-    foreach t {test_auth test_stream test_box test_secretbox test_sign} {
-        foreach l {0 10 100 1000 10000 100000 1000000} {
-            $t $l
+set count 10
+set max 10000
+set tests [list auth stream box secretbox sign]
+
+set i 0
+
+while {[string index [lindex $argv $i] 0] == "-"} {
+    if {[lindex $argv $i] == "-count"} {
+        incr i
+        set count [lindex $argv $i]
+    } elseif {[lindex $argv $i] == "-max"} {
+        incr i
+        set max [lindex $argv $i]
+    } elseif {[lindex $argv $i] == "-tests"} {
+        incr i
+        set tests [split [lindex $argv $i] " ,|"]
+    } else {
+        puts "Usage: $argv0 \[-count <n>\] \[-max <n>\] \[-tests auth|stream|box|secretbox|sign\]"
+        exit 1
+    }
+    incr i
+}
+
+if {[llength $argv] != $i} {
+    puts "Usage: $argv0 \[-count <n>\] \[-max <n>\]"
+    exit 1
+}
+
+set i 0
+set range {0}
+while {10**$i <= $max} {
+    lappend range [expr 10**$i]
+    incr i
+}
+
+for {set i 0} {$i < $count} {incr i} {
+    puts "=== Count: [expr $i + 1]"
+    foreach t $tests {
+        foreach l $range {
+            test_$t $l
         }
     }
 }
